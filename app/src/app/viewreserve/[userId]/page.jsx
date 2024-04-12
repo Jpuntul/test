93726ec131ce8314782b7fb2a97394314185798e
Jutipong <p.jutipong13@gmail.com>
2024-04-12 13:35:01 -0400

    remove duplicated code

--- app/src/app/viewreserve/[userId]/page.jsx
+++ app/src/app/viewreserve/[userId]/page.jsx
@@ -24,7 +24,7 @@ export default async function ViewReserve({ params }) {
 
   return (
     <div>
-      <div className="flex flex-col items-center justify-center gap-2 text-cente">
+      <div className="flex flex-col items-center justify-center gap-2 text-center">
         <h1 className="text-3xl font-bold text-primary tracking-tighter sm:text-5xl">Hey {user.firstName}</h1>
         <p>Here are the cars you've reserved. You can click on a reservation to view more details.</p>
       </div>      
